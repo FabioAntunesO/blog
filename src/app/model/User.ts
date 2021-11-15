2b927aa9f038724e97b8d9a7e80d075557680ab4
Fábio Antunes <fabio.fao03@gmail.com>
2021-11-15 14:50:03 -0300

    update page inicio.ts id

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -1,7 +1,7 @@
 import { Postagem } from "./Postagem"
 
 export class User {
-    public id: string
+    public id: number
     public nome: string
     public usuario: string
     public senha: string
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  id: '',
+  id: 0,
   token: '',
   nome: '',
   foto: ''
